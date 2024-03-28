98807a4854546d93a794d4aabdace7df3f562c54
Marat-Medvedev <63113518+Marat-Medvedev@users.noreply.github.com>
2024-03-28 10:50:08 +0200

    Added Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
+import Main from './components/Main'
 
 function App() {
 
   return (
-    <main className='main-container'>
-      <h1 >Portfolio</h1>
-      <p >This is a portfolio project using Vite, React, and Tailwind CSS.</p>
-    </main>
+    <Main/>
   )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+export default function Main() {
+    return (
+        <main>
+            <section>
+            <img src="../assets/images/placeholder-man.webp" alt="example photo" />
+            <h1>My name is John Doe</h1>
+            <span>Frontend developer</span>
+            <a href="#">johndoe.website</a>
+            <div>
+                <a href="#">
+                    <img src="../assets/svg/envelope-solid.svg"/>
+                    <span>Email</span>
+                </a>
+                <a href="#">
+                    <img src="../assets/svg/linkedin.svg"/>
+                    <span>LinkedIn</span>
+                </a>
+            </div>
+            </section>
+            <section>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nisl tincidunt tincidunt. Nullam ut feugiat nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nunc fermentum nunc, nec fermentum nunc nunc nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nunc fermentum nunc, nec fermentum nunc nunc nec nunc.</p>
+                <h2>Interests</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nisl tincidunt tincidunt. Nullam ut feugiat nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nunc fermentum nunc, nec fermentum nunc nunc nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nunc fermentum nunc, nec fermentum nunc nunc nec nunc.</p>
+            </section>
+        </main>
+    )
+}
